a364292a4f06bc293f5924c1cfdfa9016d114f67
Maxime Leroy <19607336+maxime1907@users.noreply.github.com>
2024-06-08 14:52:42 +0200

    fix(entrypoint): game path (#4)

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,14 +2,19 @@
 ##
 #
 # Variables
-# TZ - Setup local timezone
 # GAME_PATH - Root game path
+# STEAMCMD_PATH - SteamCMD path
 #
 ##
 
 if [ -z ${GAME_PATH} ];
 then
     export GAME_PATH=/home/container;
+fi
+
+if [ -z ${STEAMCMD_PATH} ];
+then
+    export STEAMCMD_PATH="$GAME_PATH";
 fi
 
 cd "$GAME_PATH"
@@ -23,14 +28,17 @@ export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 if [ ! -z ${SRCDS_APPID} ]; then
     if [ ! -z ${SRCDS_BETAID} ]; then
         if [ ! -z ${SRCDS_BETAPASS} ]; then
-            ./steamcmd/steamcmd.sh +login anonymous +force_install_dir /home/container +app_update ${SRCDS_APPID} -beta ${SRCDS_BETAID} -betapassword ${SRCDS_BETAPASS} +quit
+            ./$STEAMCMD_PATH/steamcmd/steamcmd.sh +login anonymous +force_install_dir "$GAME_PATH" +app_update ${SRCDS_APPID} -beta ${SRCDS_BETAID} -betapassword ${SRCDS_BETAPASS} +quit
         else
-            ./steamcmd/steamcmd.sh +login anonymous +force_install_dir /home/container +app_update ${SRCDS_APPID} -beta ${SRCDS_BETAID} +quit
+            ./$STEAMCMD_PATH/steamcmd/steamcmd.sh +login anonymous +force_install_dir "$GAME_PATH" +app_update ${SRCDS_APPID} -beta ${SRCDS_BETAID} +quit
         fi
     else
-        ./steamcmd/steamcmd.sh +login anonymous +force_install_dir /home/container +app_update ${SRCDS_APPID} +quit
+        ./$STEAMCMD_PATH/steamcmd/steamcmd.sh +login anonymous +force_install_dir "$GAME_PATH" +app_update ${SRCDS_APPID} +quit
     fi
 fi
+
+# Use system libraries
+rm "${GAME_PATH}/bin/libstdc++.so.6" | true
 
 # Replace Startup Variables
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
